262315e563fb21ab7ad3e303407b1e1627fd8914
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-06-06 18:41:49 -0300

    upload base project structure

---
+++ src/main/java/com/gabrielferreira02/timepedia/TimepediaApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielferreira02.timepedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimepediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimepediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielferreira02/timepedia/TimepediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielferreira02.timepedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimepediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
